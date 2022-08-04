28aa95eab4bb05f67b8d8732ca94acfec2f20c36
Djaysson <djayson.dev@gmail.com>
2022-08-04 16:38:47 -0300

    Style:add the global style to the project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Raleway:wght@200;500;600&display=swap"
+            rel="stylesheet"
+          />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,4 @@
+import { lighten } from 'polished';
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
@@ -5,12 +6,66 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    ::-webkit-scrollbar {
+      width: 8px;
+      height: 5px;
+    }
+    ::-webkit-scrollbar-thumb {
+      background: ${({ theme }) => theme.colors.primary};
+      border-radius: 10px;
+    }
+    ::-webkit-scrollbar-track{
+      background: ${({ theme }) => lighten(0.4, theme.colors.primary)};
+    }
+  }
+  @media (max-width: 1080px) {
+  html {
+    font-size: 93.75%; //15px
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%; //14px
+    }
   }
 
   body {
     background: ${props => props.theme.colors.background};
     color: ${props => props.theme.colors.text};
-    font: 400 16px Inter, sans-serif;
+   
   }
 
+  body,
+  input,
+  textarea,
+  button {
+    font: 400 1rem 'Lato','Raleway', sans-serif;
+  }
+
+  button {
+  cursor: pointer;
+  }
+
+  a {
+  text-decoration: none;
+  }
+
+  .container{
+    width: 100%;
+    margin:  0 auto;
+    max-width: 85rem;
+    padding: 0 calc(3.5vw + 5px);
+
+    @media(max-width:1450px){
+      max-width: 70rem;
+    }
+    @media(max-width:1080px){
+      max-width: 50rem;
+    }
+    @media(max-width:700px){
+      padding: 0 calc(3.5vw + 5px);
+    }
+  }
 `;
