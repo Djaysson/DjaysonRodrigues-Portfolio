690dfa8c53e807f57d920d1c2245948b98cd31f3
Djaysson <djayson.dev@gmail.com>
2022-09-04 16:58:19 -0300

    Feat:adding the list of the last three projects created in the Prismic CMS

--- src/components/Sections/ProjectsSection/index.tsx
+++ src/components/Sections/ProjectsSection/index.tsx
@@ -3,7 +3,18 @@ import { PageTitle } from '../../PageTitle';
 import { ProjectItems } from '../../ProjectItems';
 import { Container } from './styles';
 
-export function ProjectsSection() {
+interface ProjectData {
+  slug: string;
+  title: string;
+  category: string;
+  thumbnail: string;
+}
+
+interface ProjectsProps {
+  projects: ProjectData[];
+}
+
+export function ProjectsSection({ projects }: ProjectsProps) {
   return (
     <Container id="recentProjects">
       <PageTitle
@@ -12,24 +23,15 @@ export function ProjectsSection() {
         description="Meus trabalhos"
       />
       <section>
-        <ProjectItems
-          title="Projeto 1"
-          type="Website"
-          slug="teste"
-          img="https://miro.medium.com/max/1400/0*fH0mpJkOPO0HRy6N.jpg"
-        />
-        <ProjectItems
-          title="Projeto 2"
-          type="Website"
-          slug="teste"
-          img="https://i.pinimg.com/originals/ff/44/cd/ff44cddb751805f4bab71d8e33b5ff7d.jpg"
-        />
-        <ProjectItems
-          title="Projeto 3"
-          type="Website"
-          slug="teste"
-          img="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60107fe6084b38d8aab606b3_6002086f72b727c25f01dfaf_6hJsaUMSwwUejjp5a0zXu5BRHLhuuzdOilHegDIKPK4uw4MkYlW8qWql3-0Ey-g05SF-kXfQY1hWQ61agpkxW0FMU4w5P9QrIwqYWFWsingciUWlcdMSNIhR9uIbQ1apSL56FA0_.png"
-        />
+        {projects.slice(0, 3).map(project => (
+          <ProjectItems
+            key={project.slug}
+            title={project.title}
+            type={project.category}
+            slug={project.slug}
+            img={project.thumbnail}
+          />
+        ))}
       </section>
       <button type="button">
         <Link href="/projects">
