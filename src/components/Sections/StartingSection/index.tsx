cb8b99baa0a581ef943835c0c4f543f941c32140
Djaysson <djayson.dev@gmail.com>
2022-09-09 20:49:10 -0300

    Feat(StartingSection):making the initial section of the site responsive

--- src/components/Sections/StartingSection/index.tsx
+++ src/components/Sections/StartingSection/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FiArrowDown } from 'react-icons/fi';
 import picture from '../../../assets/images/macbook.png';
 import { Container, Content, Citation, DownArrow } from './styles';
@@ -8,18 +9,18 @@ export function StartingSection() {
       <img src={picture} alt="Photo of a Macbook" />
 
       <Content>
-        <h2>Desenvolvendo soluções em software para</h2>
+        <h2>Desenvolvendo soluções para</h2>
         <h1>
           Você & sua <span>empresa</span>{' '}
         </h1>
       </Content>
       <Citation>
         <strong>Citação</strong>
-        <i>
-          &ldquo; Existem duas maneiras de construir um projeto de software: Uma
-          é fazê-lo tão simples que obviamente não há falhas, e a outra é
-          fazê-lo tão complicado que não existem falhas óbvias. &ldquo;
-        </i>
+        <q>
+          Existem duas maneiras de construir um projeto de software: Uma é
+          fazê-lo tão simples que obviamente não há falhas, e a outra é fazê-lo
+          tão complicado que não existem falhas óbvias.{' '}
+        </q>
 
         <a
           href="https://pt.wikipedia.org/wiki/Charles_Antony_Richard_Hoare"
@@ -31,7 +32,11 @@ export function StartingSection() {
       </Citation>
 
       <DownArrow>
-        <FiArrowDown />
+        <Link href="#about">
+          <a>
+            <FiArrowDown />
+          </a>
+        </Link>
       </DownArrow>
     </Container>
   );
--- src/components/Sections/StartingSection/styles.ts
+++ src/components/Sections/StartingSection/styles.ts
@@ -1,4 +1,5 @@
 import styled, { keyframes } from 'styled-components';
+import img from '../../../assets/images/triangle.png';
 
 const animateDown = keyframes` 
   0%,
@@ -34,11 +35,20 @@ export const Container = styled.section`
   flex-direction: column;
   align-items: center;
   width: 100%;
-  height: 85vh;
-  margin-bottom: 20px;
+
+  margin-top: 1.5rem;
+
   > img {
-    width: 90vw;
+    width: 95vw;
     flex: 1;
+    filter: contrast(103%);
+  }
+
+  @media (max-width: 700px) {
+    margin-top: 6rem;
+  }
+  @media (max-width: 450px) {
+    margin-top: 8rem;
   }
 `;
 
@@ -54,9 +64,10 @@ export const Content = styled.div`
     font-size: 4rem;
     font-weight: 700;
     letter-spacing: 0.5rem;
-
+    color: ${props => props.theme.colors.primary};
     > span {
       position: relative;
+      color: ${props => props.theme.colors.text};
       &:after {
         content: '';
         height: 5px;
@@ -77,16 +88,59 @@ export const Content = styled.div`
     white-space: nowrap;
     letter-spacing: 0.2rem;
   }
+  @media (max-width: 1000px) {
+    > h1 {
+      font-size: 2.3rem;
+    }
+  }
+  @media (max-width: 700px) {
+    position: relative;
+    top: -220px;
+    padding: 0 calc(3.5vw + 5px);
+    > h1 {
+      font-size: 2.3rem;
+      white-space: normal;
+      letter-spacing: 0.1rem;
+    }
+    > h2 {
+      font-size: 1.2rem;
+      white-space: normal;
+      letter-spacing: 0.1rem;
+    }
+  }
+
+  @media (max-width: 450px) {
+    top: -220px;
+    > h1 {
+      font-size: 1.8rem;
+      white-space: normal;
+      letter-spacing: 0.1rem;
+    }
+    > h2 {
+      font-size: 1rem;
+      white-space: normal;
+      letter-spacing: 0.1rem;
+    }
+  }
+  @media (max-width: 350px) {
+    top: -170px;
+    > h1 {
+      font-size: 1.5rem;
+    }
+    > h2 {
+      font-size: 0.8rem;
+    }
+  }
 `;
 
-export const Citation = styled.div`
-  width: 25rem;
+export const Citation = styled.blockquote`
+  width: 30rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   position: relative;
-  bottom: 170px;
-  padding: 10px;
+  bottom: 160px;
+  padding: 7px 0;
 
   > strong {
     font-size: 0.9rem;
@@ -94,10 +148,11 @@ export const Citation = styled.div`
     letter-spacing: 0.2rem;
     color: ${props => props.theme.colors.primary};
   }
-  > i {
+  > q {
     font-size: 0.9rem;
     font-weight: 300;
     margin: 0.5rem 0;
+    letter-spacing: 0.05rem;
   }
   > a {
     font-size: 0.9rem;
@@ -106,9 +161,34 @@ export const Citation = styled.div`
     color: ${props => props.theme.colors.primary};
     text-align: right;
   }
+
+  @media (max-width: 1000px) {
+    width: 40rem;
+    bottom: -10px;
+  }
+  @media (max-width: 700px) {
+    width: 35rem;
+    position: relative;
+    bottom: 50px;
+  }
+  @media (max-width: 450px) {
+    width: 25rem;
+    position: relative;
+    bottom: 40px;
+    padding: 0 calc(3.5vw + 5px);
+    > q {
+      font-size: 0.8rem;
+      letter-spacing: 0.1rem;
+      margin: 0.3rem 0;
+    }
+  }
+  @media (max-width: 350px) {
+    width: 22rem;
+    bottom: 20px;
+  }
 `;
 export const DownArrow = styled.button`
-  margin-top: -150px;
+  margin-top: -100px;
   width: 40px;
   height: 40px;
   background: none;
@@ -129,5 +209,20 @@ export const DownArrow = styled.button`
     svg {
       color: ${props => props.theme.colors.text};
     }
+  }
+  @media (max-width: 1000px) {
+    position: relative;
+    top: 150px;
+  }
+  @media (max-width: 700px) {
+    position: relative;
+    top: 70px;
+  }
+  @media (max-width: 450px) {
+    position: relative;
+    top: 70px;
+  }
+  @media (max-width: 350px) {
+    top: 100px;
   }
 `;
