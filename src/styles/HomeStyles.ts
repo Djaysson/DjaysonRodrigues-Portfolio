08a2af300352fe4d36bbcb3ef3cd1b4466001bfb
Djaysson <djayson.dev@gmail.com>
2022-09-10 16:32:31 -0300

    Style:add media query in project homepage style

--- src/styles/HomeStyles.ts
+++ src/styles/HomeStyles.ts
@@ -9,6 +9,27 @@ export const HomeContainer = styled.div`
   > main {
     display: flex;
     flex-direction: column;
-    gap: 8rem;
+    gap: 5rem;
+  }
+
+  @media (max-width: 1000px) {
+    > main {
+      gap: 12rem;
+    }
+  }
+  @media (max-width: 700px) {
+    > main {
+      gap: 10.5rem;
+    }
+  }
+  @media (max-width: 450px) {
+    > main {
+      gap: 9rem;
+    }
+  }
+  @media (max-width: 350px) {
+    > main {
+      gap: 10rem;
+    }
   }
 `;
