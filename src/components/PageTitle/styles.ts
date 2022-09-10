45c493a851d570d848cda475dfc447562a210911
Djaysson <djayson.dev@gmail.com>
2022-09-10 16:33:31 -0300

    Style:make section titles responsive

--- src/components/PageTitle/styles.ts
+++ src/components/PageTitle/styles.ts
@@ -5,7 +5,6 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-
   gap: 0.2rem;
 
   > h1 {
@@ -45,6 +44,65 @@ export const Container = styled.div`
       right: -55px;
       top: 50%;
       bottom: 50%;
+    }
+  }
+  @media (max-width: 700px) {
+    > h1 {
+      font-size: 3.5rem;
+    }
+    > h2 {
+      font-size: 1.7rem;
+
+      &.pageTitle:before {
+        height: 3px;
+        width: 30px;
+        left: -35px;
+      }
+      &.pageTitle:after {
+        height: 3px;
+        width: 30px;
+        right: -35px;
+      }
+    }
+  }
+  @media (max-width: 450px) {
+    gap: 0.1rem;
+
+    > h1 {
+      font-size: 2.5rem;
+    }
+    > h2 {
+      font-size: 1.4rem;
+
+      &.pageTitle:before {
+        height: 3px;
+        width: 30px;
+        left: -35px;
+      }
+      &.pageTitle:after {
+        height: 3px;
+        width: 30px;
+        right: -35px;
+      }
+    }
+  }
+  @media (max-width: 350px) {
+    > h1 {
+      font-size: 2rem;
+    }
+    > h2 {
+      font-size: 1rem;
+
+      &.pageTitle:before {
+        height: 2px;
+        width: 30px;
+        left: -35px;
+      }
+      &.pageTitle:after {
+        height: 2px;
+        width: 30px;
+        right: -35px;
+      }
     }
   }
 `;
