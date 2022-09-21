8da4edac6656a64373743d866340b4e56f1a7723
Djaysson <djayson.dev@gmail.com>
2022-09-21 15:03:54 -0300

    Add:attach SEO meta tags to the initial project route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
 import { useEffect } from 'react';
 import Aos from 'aos';
+import Head from 'next/head';
 
 import { Header } from '../components/Header';
 import { AboutSection } from '../components/Sections/AboutSection';
@@ -22,6 +23,44 @@ export default function Home({ projects }: HomeProps) {
 
   return (
     <HomeContainer>
+      <Head>
+        <title>Home | Portfólio djayson rodrigues</title>
+        <meta
+          name="description"
+          content="Portfólio do djayson rodrigues,programador e desenvolvedor Front-end djayson rodrigues, este é meu site pessoal. Você pode dar uma olhada em meu portfólio, baixar meu CV e ver como você pode entrar em contato comigo!"
+        />
+        <meta
+          property="og:title"
+          content="Home | portfólio djayson rodrigues"
+        />
+        <meta
+          property="og:description"
+          content="Portfólio do djayson rodrigues,programador e desenvolvedor Front-end djayson rodrigues, este é meu site pessoal. Você pode dar uma olhada em meu portfólio, baixar meu CV e ver como você pode entrar em contato comigo!"
+        />
+        <meta property="og:url" content="https://djaysonrodrigues.vercel.app" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="portfólio djayson rodrigues" />
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:image:secure_url" content="/ogimage.png" />
+        <meta property="og:image:alt" content="Thumbnail" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:url" content="djaysonrodrigues.vercel.app" />
+        <meta
+          name="twitter:title"
+          content="Home | portfólio djayson rodrigues"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="portfólio djayson rodrigues" />
+        <meta name="twitter:image" content="/ogimage.png" />
+        <meta name="twitter:image:src" content="/ogimage.png" />
+        <meta name="twitter:image:alt" content="Thumbnail" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
+      </Head>
       <Header />
       <main className="container">
         <StartingSection />
